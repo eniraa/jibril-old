72bb7f9939b79ef61a3fa8f7aa3414bd0fafccb7
pyxiis <47072520+eniraa@users.noreply.github.com>
2021-12-06 18:34:42 -0500

    Add about command

--- jibril/modules/meta/commands.py
+++ jibril/modules/meta/commands.py
@@ -3,6 +3,8 @@ import math
 import hikari
 import lightbulb
 
+from utils.defaults import CONSTANTS
+
 
 @lightbulb.command("meta", "All bot-related commands")
 @lightbulb.implements(lightbulb.commands.SlashCommandGroup)
@@ -37,6 +39,45 @@ async def ping(ctx: lightbulb.context.SlashContext) -> None:
             title="🏓 Pong!",
             description=f"Latency: {ms} ms",
         )
+    )
+
+
+@meta.child
+@lightbulb.command("about", "Return information about the bot")
+@lightbulb.implements(lightbulb.commands.SlashSubCommand)
+async def about(ctx: lightbulb.context.SlashContext) -> None:
+    """Returns information about the bot
+
+    Args:
+        ctx (lightbulb.context.Context): The command's invocation context
+    """
+    devs = [
+        f"{CONSTANTS['jibril']['emoji']['developer']} "
+        + f"[{(user := await ctx.bot.rest.fetch_user(dev)).username}#"
+        + f"{user.discriminator}](https://discord.com/users/{dev})"
+        for dev in CONSTANTS["jibril"]["devs"]
+    ]
+
+    links = "\n".join(
+        [
+            "\n".join(devs),
+            f"{CONSTANTS['jibril']['emoji']['invite']} "
+            + "[Invite](https://discord.com/api/oauth2/"
+            + f"authorize?client_id={ctx.bot.get_me().id}"
+            + f"&permissions={CONSTANTS['jibril']['permissions']}"
+            + "&scope=bot%20applications.commands)",
+            f"{CONSTANTS['jibril']['emoji']['vcs']} [Repository]"
+            + f"({CONSTANTS['jibril']['uri']['repo']})",
+        ]
+    )
+
+    await ctx.respond(
+        hikari.Embed(
+            title="Jibril",
+            description=f"A Discord bot for chat games.\n\n{links}",
+        )
+        .set_thumbnail(CONSTANTS["jibril"]["assets"]["logo"])
+        .set_image(CONSTANTS["jibril"]["assets"]["banner"])
     )
 
 
